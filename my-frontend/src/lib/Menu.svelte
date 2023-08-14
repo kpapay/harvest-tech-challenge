cf87131fa2a2d7e48145cd3e3786d56359b7569f
kpapay <41725822+kpapay@users.noreply.github.com>
2023-08-15 00:23:22 +0200

    Menu.svelte file added

---
+++ my-frontend/src/lib/Menu.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  
+  import { Drawer, Button, CloseButton, Sidebar, SidebarBrand, SidebarCta, SidebarDropdownItem, SidebarDropdownWrapper, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+  import { sineIn } from 'svelte/easing';
+
+  let hidden2 = true;
+  let spanClass = 'flex-1 ml-3 whitespace-nowrap';
+  let transitionParams = {
+    x: -320,
+    duration: 200,
+    easing: sineIn
+  };
+
+</script>
+
+    <div class="absolute left-0 top-0 px-10 py-10 z-40">
+        <Button class="rounded-full" on:click={() => (hidden2 = false)}>Menu</Button>
+    </div>
+
+  <Drawer transitionType="fly" {transitionParams} bind:hidden={hidden2} id="sidebar2">
+    <div class="flex items-center">
+      <h5 id="drawer-navigation-label-3" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
+      <CloseButton on:click={() => (hidden2 = true)} class="mb-4 dark:text-white" />
+    </div>
+    <Sidebar>
+      <SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
+        <SidebarGroup>
+          <SidebarItem a href="/" label="Home">
+          </SidebarItem>
+          <SidebarItem a href="/users" label="Users">
+          </SidebarItem>
+        </SidebarGroup>
+      </SidebarWrapper>
+    </Sidebar>
+  </Drawer>
