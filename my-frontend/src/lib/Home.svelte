8fc0473fef4af6ff86d19f2c1457c9bb4aa2e99a
kpapay <41725822+kpapay@users.noreply.github.com>
2023-08-15 00:21:48 +0200

    Home.svelte file added

---
+++ my-frontend/src/lib/Home.svelte
@@ -0,0 +1,18 @@
+
+<header class="bg-blue-500 text-white absolute inset-x-0 top-0 h-16 z-30 flex items-center justify-center">
+    <span>Home page header</span>
+</header>
+
+<main>
+    <div class="grid grid-cols-1 md:grid-cols-2 h-screen w-screen absolute inset-x-0 top-0">
+      <div class="bg-blue-500 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+        <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+        <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+        <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+        <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+      </div>
+      <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+      <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+      <div class="bg-blue-500 bg-opacity-25 border-2 border-gray-300"></div>
+    </div>
+</main>
